da0c5a301541acce6cb66c6b03d48da7fc497261
LaghviAggarwal <102794818+LaghviAggarwal@users.noreply.github.com>
2024-01-02 17:23:44 +0530

    Update Login.js

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Navbar from '../components/Navbar';
 
 export default function Login() {
   const [credentials, setcredentials] = useState({ email: "", password: "" });
@@ -39,10 +40,13 @@ export default function Login() {
   };
 
   return (
+    <div style={{backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover' }}>
     <div>
+      <Navbar />
+    </div>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
+        <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+          <div className="m-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
@@ -59,7 +63,7 @@ export default function Login() {
               We'll never share your email with anyone else.
             </div>
           </div>
-          <div className="mb-3">
+          <div className="m-3">
             <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
@@ -75,7 +79,7 @@ export default function Login() {
           <button type="submit" className="m-3 btn btn-success">
             Submit
           </button>
-          <Link to="/signup" className="m-3 btn btn-danger">
+          <Link to="/signup" className="m-3 mx-1 btn btn-danger">
             I'm a new user
           </Link>
         </form>
